6321120254b28956cbd20c3f4cb569efcb3cac61
houndstack <megaampharos19@gmail.com>
2026-01-29 03:14:04 -0500

    Removed wxpython

--- app.py
+++ app.py
@@ -23,7 +23,7 @@ app = FastAPI(
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173"],  # Vite dev server
+    allow_origins=["http://localhost:5173", "https://recurate-woad.vercel.app"],  # Vite dev server
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
