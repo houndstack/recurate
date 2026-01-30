7d592f160999badceae5778cbc0fb0ae46148630
houndstack <megaampharos19@gmail.com>
2026-01-30 04:29:30 -0500

    Updated to vercel hosting

--- recurate/src/api.ts
+++ recurate/src/api.ts
@@ -1,11 +1,13 @@
 import { type Recommendation } from "./types";
 
+const API_BASE = import.meta.env.VITE_API_URL
+
 export async function fetchRecommendations(
   animeTitles: number[],
   k: number
 ): Promise<Recommendation[]> {
   console.log(animeTitles, k)
-  const res = await fetch("http://localhost:8000/recommend", {
+  const res = await fetch(`${API_BASE}/recommend`, {
     method: "POST",
     headers: { "Content-Type": "application/json" },
     body: JSON.stringify({
